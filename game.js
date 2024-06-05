c1af5ff50f4a4e30fc01559000bef2d98021378c
Andrea Rivas <andrerivas25@gmail.com>
2024-06-05 09:26:28 -0300

    funciones set canvas size y start game

--- game.js
+++ game.js
@@ -1,14 +1,38 @@
 // Crear una variable para almacenar el canva
 const canvas = document.querySelector('#game');
-
 // Darle contexto 2d
 const game = canvas.getContext('2d');
+let canvasSize;
+let elementsSize;
 
 // llamar a la funcion starGame con un addEventListener apenar cargue la pagina
-window.addEventListener('load', starGame)
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-// Crear una funcion para inicializar el juego
+function setCanvasSize() {
+    // Calcular el tamaño del canvas en función de las dimensiones de la ventana
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.7;
+    }
+
+    // Establecer el tamaño del canvas
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = (canvasSize / 10) - 1;
+
+    starGame();
+}
+
 function starGame() {
+    game.font = `${elementsSize}px Verdana`;
 
-    game.fillText('Platzi', 10, 10)
+    // Dibujar los elementos en el canvas
+    for (let i = 0; i < 10; i++) {
+        for (let z = 1; z < 11; z++) {
+            game.fillText(emojis['X'], elementsSize * i, elementsSize * z);
+        }
+    }
 }

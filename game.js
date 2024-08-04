ea7a5def840f9a16277661a89e4362f823659c8e
Andrea Rivas <andrerivas25@gmail.com>
2024-08-03 21:44:04 -0300

    set player position, and move player

--- game.js
+++ game.js
@@ -5,16 +5,87 @@ const buttonRight = document.getElementById('right');
 const buttonDown = document.getElementById('down');
 // Crear una variable para almacenar el canva
 const canvas = document.querySelector('#game');
-
 // Darle contexto 2d
 const game = canvas.getContext('2d');
 let canvasSize;
-let elementsSize;
+let elementsSize; 
+
+const playerPosition = {
+    x: undefined,
+    y: undefined
+}
+
+// llamar a la funcion starGame con un addEventListener apenar cargue la pagina
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function clearMap() {
+    game.clearRect(0, 0, canvasSize, canvasSize)
+}
+
+function setCanvasSize() {
+    // Calcular el tamaño del canvas en función de las dimensiones de la ventana
+    window.innerHeight > window.innerWidth 
+    ? canvasSize = window.innerWidth * 0.8 
+    : canvasSize = window.innerHeight * 0.7;
+    
+    // Establecer el tamaño del canvas
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = (canvasSize / 10) - 1;
+
+    startGame();
+}
+
+function startGame() {
+    clearMap();
+
+    game.font = `${elementsSize}px Verdana`;
+
+    // Crear arreglos bidimensionales 
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowsColumn = mapRows.map(row => row.trim().split(''));
+
+    // Renderizar el mapa
+    mapRowsColumn.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            const emoji = emojis[column];
+            const positionX = elementsSize * columnIndex;
+            const positionY = elementsSize * (rowIndex + 1);
+
+            if(column == 'O' && playerPosition.x == undefined) {
+                playerPosition.x = positionX;
+                playerPosition.y = positionY;
+            }
+
+            game.fillText(emoji, positionX, positionY);
+        })
+    });
+
+    movePlayer();
+}
+
+function movePlayer() {
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
+
+
 
 // Functions move character
 const move = (key) => {
     return() => {
-        console.log(`Pressed: ${key}`);
+        if (key == 'up' && playerPosition.y > 0) {
+            playerPosition.y -= elementsSize;
+        } else if (key == 'left' && playerPosition.x > 0) {
+            playerPosition.x -= elementsSize;
+        } else if (key == 'right' && playerPosition.x < canvasSize) {
+            playerPosition.x += elementsSize;
+        } else if (key == 'down' && playerPosition.y < canvasSize) {
+            playerPosition.y += elementsSize;
+        } 
+    startGame();
     }
 }
 
@@ -38,48 +109,8 @@ function moveByKeys(event) {
     }
 }
 
-// llamar a la funcion starGame con un addEventListener apenar cargue la pagina
-window.addEventListener('load', setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
 buttonUp.addEventListener('click', move('up'));
 buttonLeft.addEventListener('click', move('left'));
 buttonRight.addEventListener('click', move('right'));
 buttonDown.addEventListener('click', move('down'));
 window.addEventListener('keydown', moveByKeys);
-
-
-
-
-function setCanvasSize() {
-    // Calcular el tamaño del canvas en función de las dimensiones de la ventana
-    window.innerHeight > window.innerWidth 
-    ? canvasSize = window.innerWidth * 0.8 
-    : canvasSize = window.innerHeight * 0.7;
-    
-    // Establecer el tamaño del canvas
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
-
-    elementsSize = (canvasSize / 10) - 1;
-
-    starGame();
-}
-
-function starGame() {
-    game.font = `${elementsSize}px Verdana`;
-
-    // Crear arreglos bidimensionales 
-    const map = maps[0];
-    const mapRows = map.trim().split('\n');
-    const mapRowsColumn = mapRows.map(row => row.trim().split(''));
-
-    // Renderizar el mapa
-    mapRowsColumn.forEach((row, rowIndex) => {
-        row.forEach((column, columnIndex) => {
-            const emoji = emojis[column];
-            const positionX = elementsSize * columnIndex;
-            const positionY = elementsSize * (rowIndex + 1);
-            game.fillText(emoji, positionX, positionY);
-        })
-    });
-}
